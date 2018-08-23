9e0d8e79c39eaf2cfc43238c439c593faee8557c
Fabian Brinkmann <fabian.brinkmann@chessbase.com>
2018-08-23 15:01:50 +0200

    Registriefunktion basics

--- Server/Server/Controllers/AccountController.cs
+++ Server/Server/Controllers/AccountController.cs
@@ -8,28 +8,49 @@ using Microsoft.EntityFrameworkCore;
 using Database.Database;
 using Database.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Entities.Models;
+using Utility.Hash;
+using Database.Database.Extensions;
 
 namespace Server.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController : ControllerBase
-    {
-        private readonly TowerfallContext _context;
+	[Route( "api/v1" )]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		private readonly TowerfallContext _context;
+		private readonly int SALT_LENGTH = 512;
 
-        public AccountController(TowerfallContext context)
-        {
-            _context = context;
-            
-        }
+		public AccountController( TowerfallContext context )
+		{
+			_context = context;
 
-        // GET: api/Account
-        [HttpGet]
-        public IEnumerable<HashedPWAccount> GetAccounts()
-        {
-            return _context.Accounts;
-        }
+		}
 
-        
-    }
+		// GET: api/Account
+		[HttpGet]
+		public IEnumerable<HashedPWAccount> GetAccounts()
+		{
+			return _context.Accounts;
+		}
+
+		[Route( "login" )]
+		[HttpPost]
+		public async Task<IActionResult> Login( [FromBody] Account account )
+		{
+			//TODO Loginfunktion
+			return null;
+		}
+
+		[Route( "register" )]
+		[HttpPost]
+		public async Task<IActionResult> Register( [FromBody] Account account )
+		{
+			//TODO Registerfunktion
+			if ( !_context.Accounts.UsernameExists( account.User ) )
+				_context.Accounts.AddAccount( account, SALT_LENGTH );
+			await _context.SaveChangesAsync();
+			return null;
+		}
+	}
 }
---
+++ Server/Utility/Hash/Salt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Utility.Hash
+{
+    public class Salt
+    {
+        private static RNGCryptoServiceProvider cryptoProvider = new RNGCryptoServiceProvider();
+
+        public static string GenerateSalt(int length)
+        {
+            byte[] data = new byte[length];
+            cryptoProvider.GetBytes( data );
+            return Encoding.UTF8.GetString(data);
+        }
+    }
+}
