e4c4ecfd20d3996b6f073e0ec2f824e04cbd22c7
FSengelmann <FSengelmann20@users.noreply.github.com>
2018-08-24 11:57:06 +0200

    nicht benutzte Codeschnipsel gelöscht und letzte Kommentare eingefügt.

--- Phaser/gamelogic.js
+++ Phaser/gamelogic.js
@@ -21,19 +21,15 @@ var config = {
 };
 
 var player;
-var stars;
 var arrows;
 var walls;
 var platforms;
 var cursors;
-var score = 0;
 var gameOver = false;
-var scoreText;
 var text1;
 var right;
 var left;
 var justShot = false;
-var platformPlayerCollider;
 
 var gameWidth = 800
 var gameHeight = 600
@@ -46,6 +42,7 @@ var platformHeight = 14
 
 var game = new Phaser.Game(config);
 
+//Lädt dinge wie z.B. Bilder.
 function preload ()
 {
     // map graphics
@@ -63,6 +60,7 @@ function preload ()
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
+//Erstellt die Umgebung und weiteres.
 function create ()
 {
     //  A simple background for our game
@@ -86,14 +84,12 @@ function create ()
     //  top
     platforms.create(gameWidth / 2, gameHeight - 350, 'platform');
 
-
-
     right = false;
     left = false;
 
     // The player and its settings
     player = this.physics.add.sprite(100, 450, 'dude');
-    console.log(player);
+
     var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
     text1 = this.add.text(16, 16, 'Fritz 3', style);
     text1 = Phaser.Display.Align.To.TopCenter(text1, player, 0, 0);
@@ -140,52 +136,22 @@ function create ()
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
 
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    //stars = this.physics.add.group({
-    //    key: 'star',
-    //    repeat: 11,
-    //    setXY: { x: 12, y: 0, stepX: 70 }
-    //});
-
-    //stars.children.iterate(function (child) {
-
-        //  Give each star a slightly different bounce
-    //    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-    //});
-
     arrows = this.physics.add.group();
 
-    //  The score
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
     //  Collide the player and the stars with the platforms
-    platformPlayerCollider = this.physics.add.collider(player, platforms, null, collideInvoker, this);
-    //this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(player, platforms, null, collideInvokerPlayerPlatform, this);
 
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    //this.physics.add.overlap(player, stars, collectStar, null, this);
 
     this.physics.add.collider(player, arrows, hitArrow, null, this);
     this.physics.add.collider(platforms, arrows, arrowCollide, null, this);
 
 }
 
-function platformCollideWithPlayer (player, platform)
-{
-  //console.log(platform.texture.key == 'ground');
-  //console.log(player.body.touching);
-  if (platform.texture.key == 'platform' && platform.body.touching.down)
-  {
-    console.log('t');
-    this.physics.world.removeCollider(platformPlayerCollider);
-    setTimeout( () => {
-      //platformPlayerCollider = this.physics.add.collider(player, platforms, platformCollideWithPlayer, null, this);
-    }, 50000);
-  }
-}
-
-function collideInvoker (player, platform)
+//Wird ausgelöst wenn ein Spieler eine Platform berührt.
+//player = der Spieler
+//platform = die Platform
+function collideInvokerPlayerPlatform (player, platform)
 {
   var result = true;
   if (platform.texture.key == 'platform' && player.body.velocity.y < 0)
@@ -195,11 +161,7 @@ function collideInvoker (player, platform)
   return result;
 }
 
-function handler (gameObject)
-{
-    gameObject.tint = 0xff0000;
-}
-
+//Aktualisiert das Spiel
 function update ()
 {
     if (gameOver)
@@ -239,7 +201,6 @@ function update ()
           }
         }
         player.setVelocityX(0);
-        //player.anims.play('turn');
     }
 
     if (Phaser.Input.Keyboard.JustDown(cursors.space) && !justShot)
@@ -313,33 +274,6 @@ function arrowCollide(platform, collidedArrow)
   collidedArrow.colided = true;
   collidedArrow.body.allowGravity = false;
 }
-
-//function collectStar (player, star)
-//{
-//
-//    //  Add and update the score
-//    score += 10;
-//    scoreText.setText('Score: ' + score);
-//
-//    if (stars.countActive(true) === 0)
-//    {
-//        //  A new batch of stars to collect
-//        stars.children.iterate(function (child) {
-//
-//            child.enableBody(true, child.x, 0, true, true);
-//
-//        });
-//
-//        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-//
-//        var arrow = arrows.create(x, 16, 'bomb');
-//        arrow.setBounce(0);
-//        arrow.setCollideWorldBounds(true);
-//        arrow.setVelocity(Phaser.Math.Between(-200, 200), 20);
-//        arrow.allowGravity = false;
-//
-//    }
-//}
 
 //Wird ausgelöst wenn ein Spieler einen Pfeil berührt.
 //Wenn der Pfeil noch in bewegung ist stirbt der Spieler und wenn er liegt sammelt er ihn auf.
